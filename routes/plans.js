9c3b69092fd74272dfdc869f1dde2ae2d4ed0b95
Honey Sonia <soniaguy6@gmail.com>
2020-07-16 21:23:53 -0400

    committ

---
+++ routes/plans.js
@@ -0,0 +1,19 @@
+const { new: _new, index, show, create, edit, update, delete: _delete } = require('../controllers/PlansController');
+
+function auth (req, res, next) {
+  if (!req.isAuthenticated()) {
+    req.flash('danger', 'You need to login first.');
+    return res.redirect('/login');
+  }
+  next();
+}
+
+module.exports = router => {
+  router.get('/plans', index);
+  router.get('/plans/new', auth, _new);
+  router.post('/plans', auth, create); 
+  router.post('/plans/update', auth, update); 
+  router.post('/plans/delete', auth, _delete); 
+  router.get('/plans/:id/edit', auth, edit); 
+  router.get('/plans/:id', show);
+};
